d96e1cb16d332d6def1d14138c5ca8dcaae22f57
xzifan <xiao@zifan.dev>
2022-05-02 21:19:06 +0800

    fix: react

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const isDevelopment = process.env.NODE_ENV === 'development'
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { HotModuleReplacementPlugin } = require('webpack');
 module.exports = {
     // bundling mode
     mode: process.env.NODE_ENV,
@@ -29,5 +31,22 @@ module.exports = {
                 use: [ 'style-loader','css-loader', 'sass-loader'],
             }
         ]
+    },
+    plugins: [
+        new HotModuleReplacementPlugin({}),
+        new HtmlWebpackPlugin({
+            filename: 'index.html', // name of file that will be outputted to 'src' when built
+            template: './root.html',// path to your html file relative to config
+            inject: true
+        })
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, './'),
+            watch: true
+        },
+        port: 7000,
+        open: true,
+        compress: false
     }
 };
--- webpack.prod.js
+++
@@ -1,8 +0,0 @@
-const path = require( 'path' );
-const {merge} = require( 'webpack-merge ')
-const commonConfig = require('./webpack.common')
-module.exports = () => {
-    return merge(commonConfig, {
-        
-    })
-}
